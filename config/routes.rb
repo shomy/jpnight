54e58bce4b2a731c79492facb8575d3548d24e09
masaya0mizuno <49577793+masaya0mizuno@users.noreply.github.com>
2019-06-17 15:43:05 +0900

    offerpage.html.erb新規作成、cssファイル修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :guides
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'guides#top'
+  root 'guides#g_infos'
+
   get "/mypage" => "guides#mypage"
   get "/mypage_guest" => "guides#mypage_guest"
   get "/mypage_edit" => "guides#mypage_edit"
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
   get "/mypage_chatroom" => "guides#mypage_chatroom"
   get "/mypage_news" => "guides#mypage_news"
   get "/mypage_q" => "guides#mypage_q"
+  get "/offerpage" => "guides#offerpage"
 end

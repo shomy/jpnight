215d52dc489cf353386937d51022c5d592a42854
masaya0mizuno <49577793+masaya0mizuno@users.noreply.github.com>
2019-07-20 19:55:10 +0900

    g_infos直下へファイルを移動guidesから

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,22 @@ Rails.application.routes.draw do
   resources :guides
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'guides#g_infos'
+  root 'g_infos#top'
 
-  get "/login" => "guides#login"
-  get "/logout" => "guides#logout"
-  get "/mypage" => "guides#mypage"
-  get "/mypage_guest" => "guides#mypage_guest"
-  get "/mypage_edit" => "guides#mypage_edit"
-  get "/mypage_chat" => "guides#mypage_chat"
-  get "/mypage_chatroom" => "guides#mypage_chatroom"
-  get "/mypage_news" => "guides#mypage_news"
-  get "/mypage_q" => "guides#mypage_q"
-  get "/offerpage" => "guides#offerpage"
-  get "/guest_regi1" => "guides#guest_regi1"
-  get "/guest_regi2" => "guides#guest_regi2"
-  get "/guest_regi3" => "guides#guest_regi3"
-  get "/company" => "guides#company"
-  get "/delete" => "guides#delete"
+  get "/login" => "g_infos#login"
+  get "/logout" => "g_infos#logout"
+  get "/mypage" => "g_infos#mypage"
+  get "/mypage_guest" => "g_infos#mypage_guest"
+  get "/mypage_edit" => "g_infos#mypage_edit"
+  get "/mypage_chat" => "g_infos#mypage_chat"
+  get "/mypage_chatroom" => "g_infos#mypage_chatroom"
+  get "/mypage_news" => "g_infos#mypage_news"
+  get "/mypage_q" => "g_infos#mypage_q"
+  get "/offerpage" => "g_infos#offerpage"
+  get "/offerpage_treat" => "g_infos#offerpage_treat"
+  get "/guest_regi1" => "g_infos#guest_regi1"
+  get "/guest_regi2" => "g_infos#guest_regi2"
+  get "/guest_regi3" => "g_infos#guest_regi3"
+  get "/company" => "g_infos#company"
+  get "/delete" => "g_infos#delete"
 end

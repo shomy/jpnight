4172cb3f5ee0fa3bd44b4a1d74df4faebdd06576
masaya0mizuno <49577793+masaya0mizuno@users.noreply.github.com>
2019-08-18 23:43:05 +0900

    install rmagick,carrierwave and changed stylesheet.js, changed _form.html.erb, other css

--- app/assets/javascripts/stylesheet.js
+++ app/assets/javascripts/stylesheet.js
@@ -55,14 +55,14 @@ $(document).on ('turbolinks:load', function() {
     });
 })();
 
-$(function(){
+$(function() {
   $fileField = $('#file')
 
   // 選択された画像を取得し表示
   $($fileField).on('change', $fileField, function(e) {
     file = e.target.files[0]
     reader = new FileReader(),
-    $preview = $("#preview");
+    $preview = $("#img_field");
 
     reader.onload = (function(file) {
       return function(e) {
@@ -77,4 +77,4 @@ $(function(){
     })(file);
     reader.readAsDataURL(file);
   });
-});
+})();

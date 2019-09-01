4ca186a5eb06d4dfb7293aa0e2005ab660c7baae
masaya0mizuno <49577793+masaya0mizuno@users.noreply.github.com>
2019-09-01 20:03:22 +0900

    c

--- app/controllers/outline_info_controller.rb
+++ app/controllers/outline_info_controller.rb
@@ -2,37 +2,37 @@ class OutlineInfoController < ApplicationController
 
   def help
     @g_infos = GInfo.all
-
+    @g_info = GInfo.find_by(id: params[:id])
   end
 
   def company
     @g_infos = GInfo.all
-
+    @g_info = GInfo.find_by(id: params[:id])
   end
 
   def contact
     @g_infos = GInfo.all
-
+    @g_info = GInfo.find_by(id: params[:id])
   end
 
   def our_philosophy
     @g_infos = GInfo.all
-
+    @g_info = GInfo.find_by(id: params[:id])
   end
 
   def team
     @g_infos = GInfo.all
-
+    @g_info = GInfo.find_by(id: params[:id])
   end
 
   def code
     @g_infos = GInfo.all
-
+    @g_info = GInfo.find_by(id: params[:id])
   end
 
   def policy
     @g_infos = GInfo.all
-
+    @g_info = GInfo.find_by(id: params[:id])
   end
 
 
